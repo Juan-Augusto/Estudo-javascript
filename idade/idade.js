5bab410bb0026cc4a40fb32dce98a572905c788b
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2021-09-29 16:59:29 -0300

    Update de estilos

--- idade/idade.js
+++ idade/idade.js
@@ -3,7 +3,7 @@ function calculoIdade(){
     var anonasc = document.getElementById('anonasci')
     var idade = document.querySelector('div#idade-real')
     var maiorOuNao = document.querySelector('div#maior-de-idade')    
-    if(anoref.value.length==0 && anonasc.value.length==0){
+    if(anoref.value.length==0 || anonasc.value.length==0){
         window.alert('[ERRO] Insira os valores solicitados, por favor')
     }else{
         var ar=Number(anoref.value)
