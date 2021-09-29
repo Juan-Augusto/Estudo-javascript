d2bf3f59b27a8b95eecc3d046048adeb8fb6b92b
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2021-09-29 16:56:00 -0300

    Exercício de JS - 2
    Um simples projeto para calcular se um número inserido pelo usuário em JavaScript.

---
+++ parouimpar/parouimpar.js
@@ -0,0 +1,12 @@
+function parOuImpar(){
+    let num = document.getElementById('numcheck')
+    let checagem = document.querySelector('[check]')
+    if(num%2==0){
+        let n= Number(num.value)
+        checagem.innerHTML=`<h2>${n} é par</h2>`
+    }else{
+        let n= Number(num.value)
+        checagem.innerHTML=`<h2>${n} é impar</h2>`
+    }
+
+}
